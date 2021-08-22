6185b8096b9e4cd92c090cf941f226b5fc06a8fd
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-08-22 20:35:53 +0500

    Implemented handleToUpdate, handleToRemove, handleToCreate functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
+import TodoList from './components/TodoList';
+import {TodoInterface} from './interface'
 import './App.css';
+import { toEditorSettings } from 'typescript';
 
 function App() {
+  const [todos, setTodos] = useState<TodoInterface []>([])
+  const handleTodoCreate = (todo : TodoInterface) =>{
+    setTodos([...todos , todo ])
+  }
+
+  const handleToUpdate =  (event : React.ChangeEvent<HTMLInputElement>, id : string) =>{
+      const newTodosState : TodoInterface[] = [...todos]
+      newTodosState.find((todo : TodoInterface) => todo.id === id)!.name = event.target.value
+      setTodos([...newTodosState])
+    }
+
+  const handleTodoRemove = ( id : string ) =>{
+      const newTodosState : TodoInterface[] = 
+      todos.filter((todo: TodoInterface) => todo.id!==id)
+      console.log('todos', todos)
+      setTodos([...newTodosState])
+  }
+
+  const handleTodoComplete = ( id : string ) => {
+    const newTodosState : TodoInterface[] = [...todos] 
+    newTodosState.find((todo : TodoInterface) => todo.id === id)!.isCompleted = !newTodosState.find((todo : TodoInterface) => todo.id === id)!.isCompleted
+    setTodos([...newTodosState])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+      <React.Fragment>
+      <h2>My ToDo APP</h2>
+      <TodoForm
+      todos={todos}
+      handleTodoCreate={handleTodoCreate}
+      />
+      <TodoList
+      todos={todos}
+      handleTodoUpdate={handleToUpdate}
+      handleTodoRemove={handleTodoRemove}
+      handleTodoComplete={handleTodoComplete}
+      
+      />
+      
+      </React.Fragment>
+ </div>
   );
 }
 
